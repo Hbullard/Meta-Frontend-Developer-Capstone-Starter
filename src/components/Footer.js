52504b1ac47e1451c62945e9ebd1dd14d504f901
Hunter <hunterbullard6@gmail.com>
2025-01-22 16:57:04 -0800

    footer section finished

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,6 +11,31 @@ const Footer = () => {
                 </div>
                 <div>
                     <h3>Important Links</h3>
+                    <ul>
+                       <li><a href='/'>Home</a></li>
+                       <li><a href='/'>About</a></li>
+                       <li><a href='/'>Services</a></li>
+                       <li><a href='/'>Menu</a></li>
+                       <li><a href='/'>Reservations</a></li>
+                       <li><a href='/'>Order Online</a></li>
+                       <li><a href='/'>Login</a></li>
+                    </ul>
+                </div>
+                <div className=''>
+                    <h3>Contact</h3>
+                    <ul>
+                        <li>Address:<br/>476 Lemon Drive</li>
+                        <li>Phone:<br/>970-444-0000</li>
+                        <li>Email:<br/>littlelemon@gmail.com</li>
+                    </ul>
+                </div>
+                <div className=''>
+                    <h3>Social Media Links</h3>
+                    <ul>
+                        <li><a href='/'>Instagram</a></li>
+                        <li><a href='/'>Facebook</a></li>
+                        <li><a href='/'>Twitter</a></li>
+                    </ul>
                 </div>
             </section>
         </footer>
